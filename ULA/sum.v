2add98f929f21d970d95a9a4fa935b02085f60be
guilherme-tell <guilhermesantos2700@gmail.com>
2024-11-29 10:04:32 -0300

    Add files via upload

--- ULA/sum.v
+++ ULA/sum.v
@@ -1,6 +1,12 @@
 module sum (input [3:0]a,b,
-				output [3:0] out
+				output [3:0] out,
+				output ov
 				);
 				
-				assign out = a + b;
+				wire [4:0] out1 = a + b;
+				
+				assign ov = (out1 > 5'd15) ? 1'b1 : 1'b0;
+				
+				assign out = out1;
 endmodule
+
--- ULA/times.v
+++ ULA/times.v
@@ -1,10 +1,15 @@
 module times (input [3:0]a,b,
-							output [3:0] out
+				  output [3:0] out,
+				  output ov
 				);
 				
 				/*wire [7:0] out1 = a * b;
 				
 				assign out = out1[3:0];*/
 				
-				assign out = a*b;
+				wire [7:0] out1 = a * b;
+				
+				assign ov = (out1 > 5'd15) ? 1'b1 : 1'b0;
+				
+				assign out = out1;
 endmodule
